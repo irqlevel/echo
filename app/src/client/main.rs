73f82f4654a339e86cf69bc2015f6e41c736da5e
Andrey Smetanin <irqlevel@gmail.com>
2020-12-11 11:09:56 +0300

    more

--- app/src/client/main.rs
+++ app/src/client/main.rs
@@ -6,94 +6,51 @@ use log::{error, info, LevelFilter};
 
 extern crate common_lib;
 
-use common_lib::frame::frame::{Request, Response};
+use common_lib::frame::frame::{Request, Response, EchoRequest, EchoResponse};
 
-use common_lib::socket::socket::write as socket_write;
-use common_lib::socket::socket::read as socket_read;
 use common_lib::error::error::ServerError;
 
 use common_lib::logger::logger::SimpleLogger;
+use bincode;
 
 struct Client {
     socket: Option<tokio::net::TcpStream>
 }
 
 impl Client {
-    async fn send(&mut self, req: &mut Request) -> Result<Response, ServerError> {
-        req.header.size = req.body.len() as u32;
-        let req_header_bytes = req.header.to_bytes()?;
-
-        match self.socket.as_mut() {
-            Some(e) => {
-                socket_write(e, &req_header_bytes).await?;
-            }
-            None => {
-                return Err(ServerError::new())
-            }
-        }
-
-        match self.socket.as_mut() {
-            Some(e) => {
-                socket_write(e, &req.body).await?;
-            }
-            None => {
-                return Err(ServerError::new())
-            }
-        }
-
-        let mut resp = Response::new();
-        let mut resp_header_bytes = resp.header.to_bytes()?;
-
-        match self.socket.as_mut() {
-            Some(e) => {
-                socket_read(e, &mut resp_header_bytes).await?;
-            }
-            None => {
-                return Err(ServerError::new())
-            }
-        }
-
-        resp.header.from_bytes(&resp_header_bytes)?;
-
-        resp.body.resize(resp.header.size as usize, 0);
-
-        match self.socket.as_mut() {
-            Some(e) => {
-                socket_read(e, &mut resp.body).await?;
-            }
-            None => {
-                return Err(ServerError::new())
-            }
-        }
-
-        Ok(resp)
-    }
-
-    async fn send2<T: ?Sized, V: ?Sized>(&mut self, request: &T) -> Result<V, ServerError>
+    async fn send<T: ?Sized, V: ?Sized>(&mut self, path: &str, request: &T) -> Result<V, ServerError>
     where
         T: serde::Serialize,
         V: serde::de::DeserializeOwned
     {
-        let mut req = Request::new();
-        req.body = match bincode::serialize(request) {
-            Ok(v) => v,
-            Err(e) => {
-                error!("serialize error {}", e);
+        match self.socket.as_mut() {
+            Some(sock) => {
+                let mut rreq = Request::new(path);
+
+                rreq.body = match bincode::serialize(&request) {
+                    Ok(v) => v,
+                    Err(e) => {
+                        error!("serialize error {}", e);
+                        return Err(ServerError::new())
+                    }
+                };
+                Request::send(sock, &rreq).await?;
+
+                let rresp = Response::recv(sock).await?;
+                let resp: V = match bincode::deserialize(&rresp.body) {
+                    Ok(v) => v,
+                    Err(e) => {
+                        error!("deserialize error {}", e);
+                        return Err(ServerError::new())
+                    }
+                };
+        
+                Ok(resp)
+            }
+            None => {
                 return Err(ServerError::new())
             }
-        };
-
-        let resp = self.send(&mut req).await?;
-    
-        let decoded: V = match bincode::deserialize(&resp.body[..]) {
-            Ok(v) => v,
-            Err(e) => {
-                error!("deserialize error {}", e);
-                return Err(ServerError::new())
-            }
-        };
-
-        Ok(decoded)
+        }
     }
 
     fn new() -> Client {
@@ -133,17 +90,6 @@ impl Client {
     }
 }
 
-use serde::{Serialize, Deserialize};
-
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
-struct Entity {
-    x: f32,
-    y: f32,
-}
-
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
-struct World(Vec<Entity>);
-
 async fn test_client() -> Result<(), ServerError> {
     let addr = env::args()
     .nth(1)
@@ -153,11 +99,11 @@ async fn test_client() -> Result<(), ServerError> {
     
     client.connect(&addr).await?;
 
-    let world = World(vec![Entity { x: 0.0, y: 4.0 }, Entity { x: 10.0, y: 20.5 }]);
+    let req = EchoRequest::new();
 
-    let decoded: World = client.send2(&world).await?;
+    let resp: EchoResponse = client.send("echo", &req).await?;
 
-    assert_eq!(world, decoded);
+    assert_eq!(req.message, resp.message);
 
     client.close().await?;
 
